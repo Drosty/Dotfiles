70aca7332cb54a4f8438d22435cdf717cd6af03b
Ryan Drost <ryan@drostdev.com>
2024-03-30 14:51:09 -0500

    remove comments, and split keybinds (will use tmux)

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,6 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('JetBrains Mono', { })
@@ -10,12 +9,7 @@ config.font = wezterm.font('JetBrains Mono', { })
 config.initial_rows = 39 
 config.initial_cols = 200
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
--- config.color_scheme = "Chalk"
---
 config.ssh_domains = {
   {
     name = 'test-nix-2',
@@ -23,21 +17,13 @@ config.ssh_domains = {
     username = 'ryan'
   },
   {
-    -- This name identifies the domain
     name = 'mvtl-soil-be',
-    -- The hostname or address to connect to. Will be used to match settings
-    -- from your ssh config file
     remote_address = 'ssh-r730-mvtl-soil-be.devpod',
-    -- The username to use on the remote host
     username = 'vscode',
   },
   {
-    -- This name identifies the domain
     name = 'mvtl-soil-fe',
-    -- The hostname or address to connect to. Will be used to match settings
-    -- from your ssh config file
     remote_address = 'ssh-r730-mvtl-soil-fe.devpod',
-    -- The username to use on the remote host
     username = 'vscode',
   },
 }
@@ -57,23 +43,6 @@ config.keys = {
     key = 'RightArrow',
     mods = 'OPT',
     action = act.SendKey { key = 'f', mods = 'ALT' },
-  },
-  -- This will create a new split and run the `top` program inside it
-  {
-    key = '[',
-    mods = 'SUPER',
-    action = wezterm.action.SplitPane {
-      direction = 'Left',
-      size = { Percent = 50 },
-    },
-  },
-  {
-    key = ']',
-    mods = 'SUPER',
-    action = wezterm.action.SplitPane {
-      direction = 'Down',
-      size = { Percent = 50 },
-    },
   },
 }
 
