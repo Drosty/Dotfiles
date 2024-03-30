91ca3ebc7c44ec24e6457dbf62dc8269ddfd3c00
Ryan Drost <ryan@drostdev.com>
2024-03-30 14:45:54 -0500

    add languages to tree sitter

--- .config/nvim/lua/ryan/plugins/treesitter.lua
+++ .config/nvim/lua/ryan/plugins/treesitter.lua
@@ -3,7 +3,25 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all" (the five listed parsers should always be installed)
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+      ensure_installed = {
+        "json",
+        "xml",
+        "csv",
+        "yaml",
+        "typescript",
+        "c_sharp",
+        "apex",
+        "sql",
+        "html",
+        "css",
+
+        -- recommended to keep from treesitter
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query"
+      },
 
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
