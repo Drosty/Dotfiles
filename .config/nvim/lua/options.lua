a5b25cf2b861f6e913827a3c44dd2bd20f9994fa
Ryan Drost <ryan@drostdev.com>
2024-05-06 15:50:17 -0500

    theme and options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -8,3 +8,20 @@ require "nvchad.options"
 
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "100"
+
+
+-- 1
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format {insertSpaces = true, tabSize = 4, async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
