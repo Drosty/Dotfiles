1ee9b49bc9f4c1e196e5e020b7d39b17225b0e4f
Ryan Drost <ryan@drostdev.com>
2024-10-08 19:42:22 -0500

    update and cleanup error messages

--- .config/nvim/lua/rdev32/plugins/nvim-cmp.lua
+++ .config/nvim/lua/rdev32/plugins/nvim-cmp.lua
@@ -45,9 +45,9 @@ return {
       sources = cmp.config.sources({
         { name = "supermaven" }, -- supermaven
         { name = "nvim_lsp" }, -- lsp
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
+        -- { name = "luasnip" }, -- snippets
+        -- { name = "buffer" }, -- text within current buffer
+        -- { name = "path" }, -- file system paths
       }),
 
       -- configure lspkind for vs-code like pictograms in completion menu
